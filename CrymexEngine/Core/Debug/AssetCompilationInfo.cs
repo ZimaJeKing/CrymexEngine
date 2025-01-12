baf95a7a435cb77c7f6202375ab07649b6e53511
ZimaJeKing <retrofilip@outlook.cz>
2025-01-12 22:01:01 +0100

    Refactored most of the code, improved colliders and added more examples

---
+++ CrymexEngine/Core/Debug/AssetCompilationInfo.cs
@@ -0,0 +1,32 @@
+﻿using CrymexEngine.Debugging;
+
+namespace CrymexEngine.Debugging
+{
+    public class AssetCompilationInfo
+    {
+        public readonly float compilationTime;
+        public readonly long textureCompressedSize;
+        public readonly long audioCompressedSize;
+        public readonly long fontSize;
+        public readonly long shaderSize;
+
+        public AssetCompilationInfo(long textureCompressedSize, long audioCompressedSize, long shaderSize, long fontSize, float compilationTime)
+        {
+            this.compilationTime = compilationTime;
+            this.textureCompressedSize = textureCompressedSize;
+            this.audioCompressedSize = audioCompressedSize;
+            this.shaderSize = shaderSize;
+            this.fontSize = fontSize;
+        }
+
+        public override string ToString()
+        {
+            string final = $"Textures: {CEUtilities.ByteCountToString(UsageProfiler.TextureMemoryUsage)} raw, {CEUtilities.ByteCountToString(textureCompressedSize)} compressed\n";
+            final += $"Audio: {CEUtilities.ByteCountToString(UsageProfiler.AudioMmeoryUsage)} raw, {CEUtilities.ByteCountToString(audioCompressedSize)} compressed\n";
+            final += $"Shaders: {CEUtilities.ByteCountToString(shaderSize)}\n";
+            final += $"Fonts: {CEUtilities.ByteCountToString(fontSize)}\n";
+            final += $"Compilation Time: {CEUtilities.FloatToShortString(compilationTime, 3)} seconds";
+            return final;
+        }
+    }
+}
---
+++ CrymexEngine/Scripts/ScriptLoader.cs
@@ -0,0 +1,31 @@
+﻿using CrymexEngine.Scenes;
+using CrymexEngine.Scripts;
+using CrymexEngine.Examples;
+
+namespace CrymexEngine.Scripting
+{
+    public static class ScriptLoader
+    {
+        public static void LoadBehaviours()
+        {
+            // Add Your Behaviour Scripts Here //
+            //              |                  //
+            //              |                  //
+            //             \ /                 //
+            //              ˇ                  //
+
+            Add<CrymexEngineCompiler>();
+
+            // - - - - - - - - - - - - - - - - //
+        }
+
+        public static void Add<T>() where T : ScriptableBehaviour
+        {
+            ScriptableBehaviour? behaviour = (ScriptableBehaviour?)Activator.CreateInstance(typeof(T));
+            if (behaviour == null) return;
+
+            Scene.Current.scriptableBehaviours.Add(behaviour);
+            Behaviour.LoadBehaviour(behaviour);
+        }
+    }
+}
