3a8375b95430eaeec819077ac15041475e6c0c0f
ZimaJeKing <retrofilip@outlook.cz>
2025-02-17 08:05:28 +0100

    Fixed asset compilation

---
+++ CrymexEngine/Scripts/Behaviours/MyBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using CrymexEngine.Data;
+using CrymexEngine.Rendering;
+using CrymexEngine.UI;
+using CrymexEngine.Utils;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+
+namespace CrymexEngine.Scripts
+{
+    public class MyBehaviourScript : ScriptableBehaviour
+    {
+        UIElement display;
+        protected override void Load()
+        {
+            display = new UIElement(Assets.GetTexture("Car"), Vector2.Zero, new Vector2(500));
+        }
+
+        protected override void Update()
+        {
+            display.Rotation = Time.GameTime * 10;
+        }
+    }
+}
